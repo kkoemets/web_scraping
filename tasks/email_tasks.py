8f240d63308aacac03a638d3ee7857928f0f0f7f
kkoemets <koemets.kristjan@gmail.com>
2023-07-23 15:21:58 +0300

    Add option to ignore certain listings

--- tasks/email_tasks.py
+++ tasks/email_tasks.py
@@ -13,19 +13,22 @@ logging.basicConfig(
 
 load_dotenv()
 
+
 class IPhonePriceEmailSender:
     __emailer = Emailer()
     __parser = OunaturgParser()
     __html_creator = HtmlCreator()
     __price_threshold = int(os.getenv('EMAILING_IPHONE_PRICE_THRESHOLD', '999999'))
+    __ignored_listings = os.getenv('EMAILING_IPHONE_IGNORED_LISTINGS', ' ').split(',')
 
     def send_iphone_prices(self) -> None:
-        prices = list(
-            filter(lambda price: price.price <= self.__price_threshold,
-                   self.__parser.find_iphone_prices(IPhoneModel.IPHONE_13_PRO_MAX)))
+        listings = list(
+            filter(lambda listing: all(not listing.href.endswith(ignored) for ignored in self.__ignored_listings),
+                   list(filter(lambda listing: listing.price <= self.__price_threshold,
+                               self.__parser.find_iphone_prices(IPhoneModel.IPHONE_13_PRO_MAX)))))
 
-        if not prices:
-            logging.info('No prices found, not sending an email')
+        if not listings:
+            logging.info('No matching listings found, not sending an email')
             return
 
-        self.__emailer.send_email(self.__html_creator.create_html_table([price._asdict() for price in prices]))
+        self.__emailer.send_email(self.__html_creator.create_html_table([price._asdict() for price in listings]))
